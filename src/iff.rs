5592f314f93f3d1396727a643f9e79f5a4d59cc7
modeco80 <lily.modeco80@protonmail.ch>
2025-08-09 04:18:30 -0400

    make the iff code a bit less awful

--- src/iff.rs
+++ src/iff.rs
@@ -1,33 +1,41 @@
 //! A Offshoot of [https://docs.rs/crate/iffc]. Partially rewritten
-//! to not be a broken down overly-heap-allocating mess 
-//! (since seemingly the author did not understand newtypes), 
-//! and to not needlessly abuse operator overloading. 
-//! 
-//! Before any people who know I write C++ start, no, I 
+//! to not be a broken down overly-heap-allocating mess
+//! (since seemingly the author did not understand newtypes),
+//! and to not needlessly abuse operator overloading.
+//!
+//! Before any people who know I write C++ start, no, I
 //! don't like operator overloading abuse there either, and
 //! just because I write C++ doesn't mean I magically approve of it.
-use std::io::{IoSlice, IoSliceMut, Read, Write};
+use std::{
+    io::{IoSlice, Read, Write},
+    mem::{self, MaybeUninit},
+    slice,
+};
 
-/// An IFF chunk represents a single segment of a complete IFF
-/// file. Note: Even though this structure is capable of stroing
-/// data upto `usize` but IFF limits that to `u32` only.
-///
-/// `0` — four-byte identity of chunk.
-/// `1` — byte-data encapsulated inside it.
+/// An IFF chunk.
 #[derive(Debug, Eq, PartialEq)]
 pub struct Chunk {
-    pub fcc: [u8; 4],
+    pub fcc: u32,
     pub data: Box<[u8]>,
 }
 
-/// The size of the IFF chunk header.
-const CHUNK_HEADER_SIZE: usize = 8;
+/// An IFF chunk header.
+#[repr(C)]
+struct ChunkHeader {
+    ckid: u32,
+    size: u32,
+}
 
-/// A structure which wraps a reader and parses IFF chunks and
-/// behaves like an iterator which yields `IFFChunk` until
-/// an entire-chunk can't be constructed.
+/// The size of the IFF chunk header.
+const CHUNK_HEADER_SIZE: usize = std::mem::size_of::<ChunkHeader>();
+
+/// A newtype which wraps a [Read] and yields IFF chunks until it's
+/// not posible to parse chunks from the Read instance anymore.
 pub struct ChunkParser<T: Read>(T);
+
 impl<T: Read> ChunkParser<T> {
+    /// Create a new [ChunkParser] wrapping a given
+    /// [Read] instance.
     pub fn new(r: T) -> Self {
         Self(r)
     }
@@ -37,24 +45,28 @@ impl<T: Read> Iterator for ChunkParser<T> {
     type Item = Chunk;
 
     fn next(&mut self) -> Option<Self::Item> {
-        let mut id = [0u8; 4];
-        let mut size = [0u8; 4];
-
-        if let Err(_) = self
-            .0
-            .read_vectored(&mut [IoSliceMut::new(&mut id), IoSliceMut::new(&mut size)])
-        {
-            return None;
+        let mut header_bytes = MaybeUninit::<ChunkHeader>::uninit();
+        // SAFETY: This must initalize the whole chunk header by always reading
+        // CHUNK_HEADER_SIZE bytes, therefore this will never leave any data uninitalized.
+        let header = unsafe {
+            if let Err(_) = self.0.read_exact(slice::from_raw_parts_mut(
+                header_bytes.as_mut_ptr() as *mut u8,
+                CHUNK_HEADER_SIZE,
+            )) {
+                // An error occured trying to read, so just stop iteration.
+                return None;
+            };
+            header_bytes.assume_init()
         };
-
-        if u32::from_le_bytes(size) as usize == 0 {
-            return None;
-        }
 
         // FIXME: It might be nice policy wise to allow different modes for this.
         // However, EAC/REAL STREAM_ expects this, so it should be good enough.
-        let size = u32::from_le_bytes(size) as usize - CHUNK_HEADER_SIZE;
-        //eprintln!("size {:08x}", size);
+        let size = {
+            if header.size == 0 {
+                return None;
+            }
+            header.size as usize - CHUNK_HEADER_SIZE
+        };
         let mut data = vec![0u8; size];
 
         match self.0.read_exact(&mut data) {
@@ -66,7 +78,7 @@ impl<T: Read> Iterator for ChunkParser<T> {
         };
 
         Some(Chunk {
-            fcc: id,
+            fcc: header.ckid,
             data: data.into_boxed_slice(),
         })
     }
@@ -84,11 +96,22 @@ impl ChunkWriter {
 
     /// Appends a chunk.
     pub fn append_chunk(&mut self, chunk: Chunk) -> std::io::Result<()> {
-        let count = self.0.write_vectored(&[
-            IoSlice::new(&chunk.fcc),
-            IoSlice::new(&(chunk.data.len() as u32).to_le_bytes()[..]),
-            IoSlice::new(&chunk.data),
-        ]);
+        let chunk_header = ChunkHeader {
+            ckid: chunk.fcc,
+            size: chunk.data.len() as u32,
+        };
+
+        // SAFETY: This doesn't allow reading beyond the chunk header.
+        let chunk_header_slice = unsafe {
+            IoSlice::new(slice::from_raw_parts(
+                mem::transmute::<_, *const u8>(&chunk_header),
+                CHUNK_HEADER_SIZE,
+            ))
+        };
+
+        let count = self
+            .0
+            .write_vectored(&[chunk_header_slice, IoSlice::new(&chunk.data)]);
 
         match count {
             Ok(_) => Ok(()),
